ad3e1b046e444beda5a298f816dd3c36389b611c
justik13 <honorplay2100@gmail.com>
2026-07-16 12:53:25 +0500

    fix

--- bot/keyboards/__init__.py
+++ bot/keyboards/__init__.py
@@ -8,7 +8,7 @@ from .payment import (
     get_sbp_payment_keyboard,
 )
 from .admin.dashboard import get_admin_menu, get_audit_keyboard
-from .admin.users import get_admin_user_card_keyboard, get_admin_extend_days_keyboard
+from .admin.users import get_admin_user_card_keyboard
 from .admin.servers import get_admin_server_card_keyboard, get_server_delete_confirm_keyboard
 from .admin.tariffs import get_admin_tariff_card_keyboard
 from .admin.broadcast import get_broadcast_confirm_keyboard
@@ -22,7 +22,7 @@ __all__ = [
     "get_payment_method_keyboard", "get_payment_success_keyboard",
     "get_sbp_payment_keyboard",
     "get_admin_menu", "get_audit_keyboard",
-    "get_admin_user_card_keyboard", "get_admin_extend_days_keyboard",
+    "get_admin_user_card_keyboard",
     "get_admin_server_card_keyboard", "get_server_delete_confirm_keyboard",
     "get_admin_tariff_card_keyboard",
     "get_broadcast_confirm_keyboard",
